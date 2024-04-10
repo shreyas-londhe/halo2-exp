504c505945ba879bbc1d0cc3ff723dafe29900ac
shreyas-londhe <shreyas_londhe@outlook.com>
2024-04-10 18:07:00 +0530

    fix: incorrect modulus

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@ mod test {
 
         // BN254 modulus
         let modulus = BigUint::from_str_radix(
-            "30644e72e131a029b85045b68181585d28333e84879b9709143e1f593f0000001",
+            "30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001",
             16
         ).unwrap();
 
